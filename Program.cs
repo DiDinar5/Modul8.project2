20590e1c7851d5d9d0fddc525ea796d072c53aeb
Dinar <dinar-wp9@mail.ru>
2022-11-07 16:33:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DelegatePractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string folderPath = Console.ReadLine();
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            Console.WriteLine(di.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length));
+
+            Console.Read();
+        }
+    }
+}
